4b52b31865d98ce0b77671a70381b7c2c8a020aa
ImMega <72713009+ImMega@users.noreply.github.com>
2020-10-13 23:55:53 +0200

    Update ppsize.js

--- commands/ppsize.js
+++ commands/ppsize.js
@@ -3,7 +3,9 @@ module.exports = {
     description: "Velicina malog" ,
     execute(message, args){
         const target = message.mentions.users.first()
+
         const ppsize = Math.floor(Math.random() * 50)
+        
         if (!target){
             message.reply('ti imas ' + `${ppsize}` + 'inch velikog!');
         } else if (target){
