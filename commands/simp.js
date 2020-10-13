fe8f2fece884e8b1b99be24e9d6cda47c4781143
ImMega <72713009+ImMega@users.noreply.github.com>
2020-10-13 23:59:45 +0200

    Update simp.js

--- commands/simp.js
+++ commands/simp.js
@@ -3,13 +3,13 @@ module.exports = {
     description: "Koliki si simp" ,
     execute(message, args){
         const target = message.mentions.users.first()
-        
+
         const simp = Math.floor(Math.random() * 100)
 
         if (!target){
             message.reply('ti si ' + `${simp}` + '% simp!');
         } else if (target){
-            message.channel.send(`${target}` + "je " + `${simp}` + '% simp!');
+            message.channel.send(`${target}` + " je " + `${simp}` + '% simp!');
         }
     }
 }
