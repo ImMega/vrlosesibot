9006c7bffb91754af24707936bdc40406b27efc8
ImMega <72713009+ImMega@users.noreply.github.com>
2020-10-14 00:17:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -73,7 +73,20 @@ client.on("message", message =>{
       } else if(command === "ppsize"){
           client.commands.get("ppsize").execute(message, args);
       } else if(command === "help"){
-        client.commands.get("help").execute(message, args);
+        const helpEmbed = new Discord.RichEmbed()
+            .setColor(0x6509ed)
+            .setTitle(`Podrzane komande`)
+            .setDescription(`${prefix} ` + commands.map(c => c.name).join(`\n${prefix} `))
+            .setAuthor(message.author.username)
+            .setField(`To su te komande!`)
+            .setThumbnail(message.author.avatarURL)
+            .setFooter(`Da, to su te komande!`)
+            .setTimestamp()
+        try {
+            message.channel.sendEmbed(helpEmbed);
+        } catch {
+            message.reply(`Nesto ne radi`)
+        }
     }
 });
 
